ccf6b5805a4d57a4f5827d1a80adc26242561b12
Yossef-Elshafey <yssef0472@gmail.com>
2024-09-11 19:44:29 +0300

    login first steps functionality

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  loginForm = new FormGroup({
+    user: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    password: new FormControl('', [Validators.required]),
+  });
 
+  submitCreds() {
+    const user = this.loginForm.value.user ?? '';
+    const password = this.loginForm.value.password ?? '';
+
+    console.log('User:', user);
+    console.log('Password:', password);
+  }
 }
