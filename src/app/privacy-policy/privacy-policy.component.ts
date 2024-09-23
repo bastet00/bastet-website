079ec7cad4e9e0315d023b57c6f5c57507bd0dd3
Amro <amranwar945@gmail.com>
2024-09-23 23:08:56 +0200

    Add privacy-policy

---
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+
+@Component({
+  selector: 'app-privacy-policy',
+  standalone: true,
+  imports: [NavbarComponent],
+  templateUrl: './privacy-policy.component.html',
+  styleUrl: './privacy-policy.component.scss',
+})
+export class PrivacyPolicyComponent {}
