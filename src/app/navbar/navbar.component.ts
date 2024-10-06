aff90fddcf12ff7df6757a8ef4864bf7e582284a
Amro <amranwar945@gmail.com>
2024-10-06 13:59:59 +0200

    Add email

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -5,8 +5,15 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
+  mailBody = `𓃠 للتواصل مع  باستيت %0A
+  %0A
+  الموضوع:%0A
 
+  %0A
+  الاسم:%0A
+  رقم الهاتف:%0A
+  `;
 }
