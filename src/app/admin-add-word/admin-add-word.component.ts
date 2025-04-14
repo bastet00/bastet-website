f02dad80b18b83be87689b692213a901230b78f6
Yossef-Elshafey <yssef0472@gmail.com>
2025-04-14 09:54:38 +0200

    dashboard layout

--- src/app/admin-add-word/admin-add-word.component.ts
+++ src/app/admin-add-word/admin-add-word.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { LoginService } from '../services/login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-add-word',
   standalone: true,
   imports: [],
   templateUrl: './admin-add-word.component.html',
-  styleUrl: './admin-add-word.component.scss'
+  styleUrl: './admin-add-word.component.scss',
 })
 export class AdminAddWordComponent {
+  constructor(
+    private loginService: LoginService,
+    private router: Router,
+  ) {}
 
+  ngOnInit(): void {
+    this.loginService.isTrusted$.subscribe((val) => {
+      if (!val) {
+        this.router.navigateByUrl('/');
+      }
+    });
+  }
 }
